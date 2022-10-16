841cf5e053e78d03fc57a47ab10889012c4c8545
buzhengshi <buzhengshi@gmail.com>
2022-10-16 11:51:27 +0800

    property提交

---
+++ class.py
@@ -0,0 +1,12 @@
+class test():
+	def __init__(self,name):
+		self.name=name
+	def getname(self):
+		return self.name
+	def setname(self,name):
+		self.name = name
+	na = property(getname,setname)
+
+te=test('掌扇三')
+print(te.na)
+		
